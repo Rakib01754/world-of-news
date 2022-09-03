73e3ee45e6b0c1d42a248d52cd858bce0ab18550
Rakib01754 <rakibul.freelancer567@gmail.com>
2022-09-03 11:33:04 +0600

    display category added on js and index

--- app.js
+++ app.js
@@ -1,8 +1,30 @@
 // Load Catagories Part
 
 const loadCategory = async () => {
-    const response = await fetch('https://openapi.programming-hero.com/api/news/categories')
-    const data = await (response.json())
-    const catagories = await (data.data.news_category);
-    return catagories;
+    try {
+        const response = await fetch('https://openapi.programming-hero.com/api/news/categories')
+        const data = await (response.json())
+        const categories = await (data.data.news_category);
+        return categories
+    } catch (error) {
+        console.log(error)
+    }
 }
+
+// Display Catagories Part 
+
+const displayCategories = async () => {
+    const categories = await (loadCategory());
+    const categoriesContainer = document.getElementById('category-container');
+    categories.forEach(category => {
+        const { category_name, category_id } = category
+        const categoryLi = document.createElement('li');
+        categoryLi.classList.add('font-bold', 'mx-auto')
+        categoryLi.innerHTML = `
+        <a onclick="displayNewses('${category_id}','${category_name}')">${category_name}</a>
+        `
+        categoriesContainer.appendChild(categoryLi)
+    });
+}
+
+displayCategories();
